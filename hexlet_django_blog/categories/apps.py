94874bb35c4aa57523d26eb5b4029d9ef39f09c2
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-15 14:44:34 +0300

    Update apps.py

--- hexlet_django_blog/categories/apps.py
+++ hexlet_django_blog/categories/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CategoriesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'categories'
+    name = 'hexlet_django_blog.categories'
