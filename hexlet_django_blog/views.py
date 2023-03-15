c7d11e1e7f11d4b1587824f33e08cc74f7c8b3cb
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-15 14:50:57 +0300

    Update views.py

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html', context={
-        'who': 'World',
-    })
-
+    return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
