fe7b4793d893f242a2ef18407f919f8d08c2bdca
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-17 16:38:08 +0300

    Update settings.py

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'hexlet_django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates/articles'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
