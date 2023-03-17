b1689af2c8da941541ac29429ee456355682a9f7
LeonidBabkin <Babkinleo@gmail.com>
2023-03-17 16:15:31 +0300

    settings.py

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'hexlet_django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates', 'articles': '/hexlet-django-blog/hexlet_django_blog/templates/articles'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
