b732288d47fc2d23167edbfc7a269b425fdaa74f
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-18 11:56:49 +0300

    Update views.py

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
+TEAM = [
+    {'name': 'death-metal-bats', 'author': 'Elizabeth Rayne', 'link': 'https://www.livescience.com/death-metal-bats'},
+    {'name': 'mind-controlled-wolves-toxoplasma-gondii', 'author': 'Joshua A. Krisch ', 'link': 'https://www.livescience.com/mind-controlled-wolves-toxoplasma-gondii'},
+    {'name': 'wild-chimpanzees-and-gorillas-can-form-friendships', 'author': 'Chris Young', 'link': 'https://www.livescience.com/wild-chimpanzees-and-gorillas-can-form-friendships'},
+    {'name': 'longest-living-animals', 'author': 'Patrick Pester', 'link': 'https://www.livescience.com/longest-living-animals.html'},
+]
+
+
 def index(request):
     return render(request, 'index.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(
+        request,
+        'about.html',
+        context={'TEAM': TEAM},
+    )
