201277d0e85826328c0327738c732fe7e5995992
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-18 17:06:56 +0300

    Update views.py

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'article/templates/index.html', context={'title': 'Некоторая статья'})
+    return render(request, 'hexlet-django-blog/hexlet_django_blog/article/templates/index.html', context={'title': 'Некоторая статья'})
