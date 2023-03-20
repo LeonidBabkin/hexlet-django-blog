ab5d1b359e91c66cf6cb51b7c24fd6ec9db684b2
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-20 11:52:25 +0300

    Update views.py

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView
 
 ARTICLES = [
     {'name': 'death-metal-bats', 'author': 'Elizabeth Rayne', 'link': 'https://www.livescience.com/death-metal-bats'},
@@ -8,10 +8,12 @@ ARTICLES = [
     {'name': 'longest-living-animals', 'author': 'Patrick Pester', 'link': 'https://www.livescience.com/longest-living-animals.html'},
 ]
 
-
-def index(request):
-    return render(request, 'index.html')
-
+class IndexView(TemplateView):
+    template_name = "index.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['who'] = 'World'
+        return context
 
 def about(request):
     tags = ['обучение', 'программирование', 'python', 'oop']
