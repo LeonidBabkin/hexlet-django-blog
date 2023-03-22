c4e34caa70a2da744618226eeafee343b4bd13de
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-22 13:59:03 +0300

    Update views.py

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import redirect
 from django.urls import reverse
 
 
