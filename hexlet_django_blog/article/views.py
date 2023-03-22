dc52d746a169dbf4705ced8525cec52b366a78d1
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-22 12:39:18 +0300

    Update views.py

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 
 #  Переделайте hexlet_django_blog.article.views.index в класс-потомок от View. В дальнейшем мы будем расширять этот класс
 # class ArticleIndexView(View):
@@ -10,4 +12,5 @@ from django.http import HttpResponse
 #         return render(request, self.template_name, context={'title': 'Некоторая статья'})
 def index(request, tags, article_id):
     template_name = "article/index.html"
-    return HttpResponse(f'Статья номер {article_id}. Тег {tags}')
+    redirect_url = reverse('article', args=(article_id, tags)
+    return HttpResponseRedirect(redirect_url)
