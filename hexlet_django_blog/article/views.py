59e3828dc18e48c2e21d51fc886bc542eab62456
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-22 11:40:02 +0300

    Update views.py

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.views import View
 
 #  Переделайте hexlet_django_blog.article.views.index в класс-потомок от View. В дальнейшем мы будем расширять этот класс
-class ArticleIndexView(View):
-    template_name = "article/index.html"
+# class ArticleIndexView(View):
+#     template_name = "article/index.html"
 
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, context={'title': 'Некоторая статья'})
+#     def get(self, request, *args, **kwargs):
+#         return render(request, self.template_name, context={'title': 'Некоторая статья'})
+def index(request):
+    template_name = "article/index.html"
+    return render(request, template_name, 
