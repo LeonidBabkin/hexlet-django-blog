247e202a919d90dd0d560f4a61192e8d9b69fae2
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-23 11:53:51 +0300

    Update views.py

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
-from django.http import HttpResponseRedirect
+# from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 
@@ -15,11 +15,14 @@ ARTICLES = [
 #  а не быть заменена на TemplateView.as_view(…)
 
 class IndexView(TemplateView):
-    template_name = "index.html"
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['who'] = 'World'
-        return context
+    def get(self, request):
+        return redirect(reverse('article', tags='python', article_id=42))
+
+#     template_name = "index.html"
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['who'] = 'World'
+#         return context
 
 def about(request):
     tags = ['обучение', 'программирование', 'python', 'oop']
