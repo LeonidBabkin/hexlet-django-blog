3235b41f9ecb7e1f4c3b3b5338f068b3a2eb0880
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-23 11:15:46 +0300

    Update models.py

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Article(models.Model):
   title = models.TextField()
   content = models.TextField()
+  
+  
+  def get_absolute_urls(self,):
+      
+    
+
  
