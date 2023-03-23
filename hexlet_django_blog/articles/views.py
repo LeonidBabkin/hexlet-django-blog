2b7c08fce7918c781ecd92c1aa2bed48478950b2
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-23 16:13:46 +0300

    Update views.py

--- hexlet_django_blog/articles/views.py
+++ hexlet_django_blog/articles/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.views.decorators.http import require_http_methods
 
+
+articles = [
+    {'id': 1, 'title': '"How to foo?"', 'author': 'F. BarBaz'},
+    {'id': 2, 'title': '"Force 101"', 'author': 'O-W. Kenobi'},
+    {'id': 3, 'title': '"Top 10 skyscrapers"', 'author': 'K. Kong'},
+    {'id': 4, 'title': '"Top 10 skyscrapers (jp. edition)"', 'author': 'K. Godzilla'},
+    {'id': 5, 'title': '"5 min recepies"', 'author': 'H. Lector'},
+]
 # Create your views here.
 def articles(request):
     return render(request, 'articles.html')
+
+
+@require_http_methods(['GET'])
+def article_id(request, id):
+    for article in articles:
+        if article['id'] == id:
+            return render(request, 'articles/article.html', context={'article': article})
+    raise Http404()
+
+
+
