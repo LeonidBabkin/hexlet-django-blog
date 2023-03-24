e95e9cf763010b01dc6e9a1c4376a82ad026d1a3
LeonidBabkin <Babkinleo@gmail.com>
2023-03-24 11:33:17 +0300

    model

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -7,10 +7,3 @@ class Article(models.Model):
     name = models.CharField(max_length=200) # название статьи
     body = models.TextField() # тело статьи
     timestamp = models.DateTimeField(auto_now_add=True)
-  
-
-def get_absolute_urls(self,):
-    return reverse('article', args=[self.pk])
-    
-
- 
