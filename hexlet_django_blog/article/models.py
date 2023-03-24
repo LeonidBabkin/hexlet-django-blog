46f57fea91bb90e5586bb045b13063d5f85b5a30
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-24 10:52:39 +0300

    Update models.py

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -3,8 +3,9 @@ from django.urls import reverse
 
 # Create your models here.
 class Article(models.Model):
-  title = models.TextField()
-  content = models.TextField()
+    name = models.CharField(max_length=200) # название статьи
+    body = models.TextField() # тело статьи
+    timestamp = models.DateTimeField(auto_now_add=True)
   
   
   def get_absolute_urls(self,):
