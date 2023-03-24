c01a74c3987ed92055a8298728cf6c6ce57ac14a
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-24 17:48:03 +0300

    Update models.py

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 class Article(models.Model):
     name = models.CharField(max_length=200) # название статьи
     body = models.TextField() # тело статьи
-    created_at = models.DateTimeField(timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
 # auto_now_add=True can be used as well
     
     def __str__(self):
