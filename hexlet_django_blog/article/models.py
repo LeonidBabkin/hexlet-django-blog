972d86a76a70bb81b25f6f9fa789dafdee929960
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-24 17:32:00 +0300

    Update models.py

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -6,4 +6,8 @@ from django.urls import reverse
 class Article(models.Model):
     name = models.CharField(max_length=200) # название статьи
     body = models.TextField() # тело статьи
-    timestamp = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
+    def __str__(self):
+        return self.name
