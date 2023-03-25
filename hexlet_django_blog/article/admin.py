075dbfb71b51f2f254e5422ed50cc1f934d5cf2c
LeonidBabkin <Babkinleo@gmail.com>
2023-03-25 11:10:37 +0300

    class ArticleAdmin(admin.ModelAdmin):

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+        search_fields = ['name', 'body']
+
+admin.site.register(Article, ArticleAdmin)
 
 # Register your models here.
