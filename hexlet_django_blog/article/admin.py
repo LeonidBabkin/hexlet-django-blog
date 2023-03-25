16273fc57312a74a40253d4a67ba21d4ec83bcdb
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-25 11:12:52 +0300

    Update admin.py

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from .models import Article
 
+# Мы добавили класс, который описывает дополнительные свойства для отображения и работы с нашей моделью в разделе 
+# администратора. В данном случае мы указали поле search_fields, в которое передали списком названия полей. 
+# По ним будет осуществляться поиск.
 
 class ArticleAdmin(admin.ModelAdmin):
         search_fields = ['name', 'body']
-
+        
+# Register your models here.
 admin.site.register(Article, ArticleAdmin)
 
-# Register your models here.
+
