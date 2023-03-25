1cb3581096708df3e90cc5bc5277257054ec4db8
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-25 13:10:22 +0300

    Update urls.py

--- hexlet_django_blog/articles/urls.py
+++ hexlet_django_blog/articles/urls.py
@@ -3,6 +3,6 @@ from hexlet_django_blog.articles import views
 
 
 urlpatterns = [
-    path('', views.articles, name='app-views-articles'),
+    path('', IndexView.as_view(), name='app-views-articles'),
     path('<int:id>/', views.article_id, name='article_id'),
 ]
