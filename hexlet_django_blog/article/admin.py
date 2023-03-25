a31d16936d2a9d8d0d510cf6cd850bb2aff38bef
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-25 11:20:30 +0300

    Update admin.py

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -5,10 +5,21 @@ from .models import Article
 # администратора. В данном случае мы указали поле search_fields, в которое передали списком названия полей. 
 # По ним будет осуществляться поиск.
 
-class ArticleAdmin(admin.ModelAdmin):
-        search_fields = ['name', 'body']
+# class ArticleAdmin(admin.ModelAdmin):
+#         search_fields = ['name', 'body']
         
-# Register your models here.
-admin.site.register(Article, ArticleAdmin)
+# # Register your models here.
+# admin.site.register(Article, ArticleAdmin)
 
 
+# Мы можем улучшить читаемость нашего кода, если воспользуемся декоратором @admin.register(). 
+# Он позволяет связать модель с классом и провести регистрацию модели в разделе администратора:
+# Еще добавим отображение в списке статей даты публикации и фильтрацию по данному полю:
+from django.contrib.admin import DateFieldListFilter
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):  
+        list_display = ('name', 'timestamp') # Перечисляем поля, отображаемые в таблице списка статей
+        search_fields = ['name', 'body']
+        list_filter = (('timestamp', DateFieldListFilter),) # Перечисляем поля для фильтрации
