4eeaaf6017d55b016877c06aefb7113071114958
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-25 13:50:51 +0300

    Update views.py

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -5,7 +5,7 @@ from hexlet_django_blog.article.models import Article
 
 #  Переделайте hexlet_django_blog.article.views.index в класс-потомок от View. 
 #  В дальнейшем мы будем расширять этот класс
-class ArticleIndexView(View):
+class ArticleIdView(View):
     template_name = "article/index.html"
 
     def get(self, request, *args, **kwargs):
@@ -14,6 +14,14 @@ class ArticleIndexView(View):
             'article': article,
         })
 
+
+class ArticleFirstView(View):
+    template_name = "index.html"
+    def get(self, request, *args, **kwargs):
+        article = Article.objects.first()
+        return render(request, self.template_name, context={
+            'article': article,
+        })
 
 # Сделайте так, чтобы hexlet_django_blog.article.views.index принимала строковый параметр "tags" и 
 # целочисленный параметр "article_id" из пути /article/tags/article_id и выводила текст в виде Статья номер 42. 
