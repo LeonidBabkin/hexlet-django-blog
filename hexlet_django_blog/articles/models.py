f1e27c47280839760103b85cf882d1b58a13121d
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-26 12:45:35 +0300

    Update models.py

--- hexlet_django_blog/articles/models.py
+++ hexlet_django_blog/articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from hexlet-django-blog.categories.models import Category
+
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField('title', max_length=255)
+    body = models.CharField('body', max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        """Represent model object."""
+        return self.title
