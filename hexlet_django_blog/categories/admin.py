0074cb3ab17f49c0246c11bf3409f3d366d494fa
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-26 14:54:41 +0300

    Update admin.py

--- hexlet_django_blog/categories/admin.py
+++ hexlet_django_blog/categories/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Category 
 # Register your models here.
+
+ 
+@admin.register(Category) 
+class CategoryAdmin(admin.ModelAdmin): 
+    list_display = ('name', 'description') # Перечисляем поля, отображаемые в таблице списка статей 
+    search_fields = ['name', 'description'] 
