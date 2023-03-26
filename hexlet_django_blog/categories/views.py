7772f3181549375723c58698ceb4d2bd14fe156a
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-26 13:00:25 +0300

    Update views.py

--- hexlet_django_blog/categories/views.py
+++ hexlet_django_blog/categories/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.views import View
+from hexlet_django_blog.categories.models import Category
 
-# Create your views here.
-def categories(request):
-    return render(request, 'categories.html')
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.all()
+        return render(request, 'categories/index.html', context={
+            'categories': categories,
+        })
+
+
+# BEGIN (write your solution here) Task 13
+class CategoryView(View):
+    template_name = 'categories/category.html'
+
+    def get(self, request, *args, **kwargs):
+        category = get_object_or_404(Category, id=kwargs['id'])
+        return render(request, self.template_name, context={
+            'category': category,
+        })
+# END
