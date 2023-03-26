4119d53df9499819dc184a4662c0b2a9baf35fc0
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-26 12:57:13 +0300

    Update models.py

--- hexlet_django_blog/categories/models.py
+++ hexlet_django_blog/categories/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField('name', max_length=255)
+    description = models.CharField('description', max_length=255)
+
+    def __str__(self):
+        """Represent model object."""
+        return self.name
