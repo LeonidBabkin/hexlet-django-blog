6ecde78d45fd0ac0ba1291ec60cddaffca2db82f
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-26 13:25:57 +0300

    Update views.py

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,51 +1,8 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
-# from django.shortcuts import redirect
-# from django.http import HttpResponseRedirect
-# from django.urls import reverse
 
 
-ARTICLES = [
-    {'name': 'death-metal-bats', 'author': 'Elizabeth Rayne', 'link': 'https://www.livescience.com/death-metal-bats'},
-    {'name': 'mind-controlled-wolves-toxoplasma-gondii', 'author': 'Joshua A. Krisch ', 'link': 'https://www.livescience.com/mind-controlled-wolves-toxoplasma-gondii'},
-    {'name': 'wild-chimpanzees-and-gorillas-can-form-friendships', 'author': 'Chris Young', 'link': 'https://www.livescience.com/wild-chimpanzees-and-gorillas-can-form-friendships'},
-    {'name': 'longest-living-animals', 'author': 'Patrick Pester', 'link': 'https://www.livescience.com/longest-living-animals.html'},
-]
-
-#  Переделайте hexlet_django_blog.views.index с использованием TemplateView. View должна наследовать этот класс,
-#  а не быть заменена на TemplateView.as_view(…)
-
-class IndexView(TemplateView):
-#  Сделайте так, чтобы открытие "домашней страницы" делало перенаправление на /article/python/42. 
-#  Для этого используйте django.shortcuts.redirect и django.urls.reverse. Не задавайте URL напрямую, используйте обратный 
-#  маршрут
-#     def get(self, request):
-#         return redirect(reverse('article', kwargs={'tags':'python', 'article_id':42}))
-    template_name = "index.html"
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['who'] = 'World'
-        return context
+def index(request):
+    return render(request, 'index.html')
 
 def about(request):
-    tags = ['обучение', 'программирование', 'python', 'oop']
-    return render(
-        request,
-        'about.html',
-        context={'tags': tags},
-    )
-
-
-# def details(request):
-#     return render(
-#         request,
-#         'details.html',
-#         context={'ARTICLES': ARTICLES},
-#     )
-
-class DetailsView(TemplateView):
-    template_name = "details.html"
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context={'ARTICLES': ARTICLES}
-        return context
+    return render(request, 'about.html')
