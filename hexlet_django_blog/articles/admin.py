3105fc30414460d8a02b9d6126eeddcc4a7fbdc7
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-26 14:26:09 +0300

    Update admin.py

--- hexlet_django_blog/articles/admin.py
+++ hexlet_django_blog/articles/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from .models import Article 
 
 # Register your models here.
-admin.site.register(Article)
+# admin.site.register(Article)
+
+# class ArticleAdmin(admin.ModelAdmin): 
+#     search_fields = ['name', 'body'] 
+ 
+# admin.site.register(Article, ArticleAdmin) 
+@admin.register(Article) 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'timestamp')
+    search_fields = ['name', 'body']
+    list_filter = (('created_at', DateFieldListFilter),)
+    
+
