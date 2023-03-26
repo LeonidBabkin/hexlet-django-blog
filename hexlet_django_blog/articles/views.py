5b9b65d26cab9aeee2380a2e6c7f7b3ae7ebacfd
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2023-03-26 12:49:27 +0300

    Update views.py

--- hexlet_django_blog/articles/views.py
+++ hexlet_django_blog/articles/views.py
@@ -1,35 +1,25 @@
-from django.shortcuts import render, redirect
-from django.http import Http404
-from django.views.decorators.http import require_http_methods
-from hexlet_django_blog.article.models import Article
+from django.shortcuts import get_object_or_404, render
 from django.views import View
-
-articles_list = [
-    {'id': 1, 'title': '"How to foo?"', 'author': 'F. BarBaz'},
-    {'id': 2, 'title': '"Force 101"', 'author': 'O-W. Kenobi'},
-    {'id': 3, 'title': '"Top 10 skyscrapers"', 'author': 'K. Kong'},
-    {'id': 4, 'title': '"Top 10 skyscrapers (jp. edition)"', 'author': 'K. Godzilla'},
-    {'id': 5, 'title': '"5 min recepies"', 'author': 'H. Lector'},
-]
-# Create your views here.
+from hexlet-django-blog.articles.models import Article
 
 
-#  Переделайте hexlet_django_blog.article.views.index в класс-потомок от View. 
-#  В дальнейшем мы будем расширять этот класс
 class IndexView(View):
     template_name = 'articles/index.html'
 
     def get(self, request, *args, **kwargs):
-        articles = Article.objects.all()[:15]  # Первой строкой из базы извлекаются 15 первых статей. Django автоматически определяет наличие размера списка в запросе и выполняет правильное смещение в SQL.
+        articles = Article.objects.all()
         return render(request, self.template_name, context={
             'articles': articles,
         })
 
 
+class ArticleView(View):
+    template_name = 'articles/article.html'
 
-@require_http_methods(['GET'])
-def article_id(request, id):
-    for article in articles_list:
-        if article['id'] == id:
-            return render(request, 'articles.html', context={'article': article})
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, self.template_name, context={
+            'article': article,
+        })
+
     raise Http404()
