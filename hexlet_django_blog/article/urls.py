88e6047f6d7350f3847c5f5021972e73209fc70b
LeonidBabkin <Babkinleo@gmail.com>
2023-03-27 10:50:52 +0300

    article urls.py

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from hexlet_django_blog.article.views import CommentArticleView
+from hexlet_django_blog.article.views import CommentFormView
 
 urlpatterns = [
-    path('', CommentArticleView.as_view(), name='comment_create'),
+    path('', CommentFormView.as_view(), name='comment_create'),
 ]
